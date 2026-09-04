578d97011c9a02a528ead40aee2ae22ef2aab0af
Sukanya Srimanthula <srimanthulachandramouli0@gmail.com>
2026-09-04 06:29:43 +0530

    Update Import.py

--- Import.py
+++ Import.py
@@ -39,3 +39,22 @@ print("\n-------------------------------------")
 print("Final UFW Status: sudo ufw status")
 print("23 DENY | 22 ALLOW | Default: DENY")
 print("Firewall Log: All threats blocked!")
+
+#Out put
+=====================================
+  UFW FIREWALL SIMULATION - ACTIVE
+=====================================
+Applying Rules: sudo ufw enable
+
+[06:28:15] Port 23 -> BLOCK (Telnet - Insecure) -> Packet   DROPPED (Blocked by Firewall)
+[06:28:16] Port 22 -> ALLOW (SSH - Secure) -> Packet ✅ PASSED
+[06:28:17] Port 80 -> ALLOW (HTTP) -> Packet ✅ PASSED
+[06:28:17] Port 443 -> ALLOW (HTTPS) -> Packet ✅ PASSED
+[06:28:18] Port 3389 -> BLOCK (Default Deny Policy) -> Packet ❌ DROPPED (Blocked by Firewall)
+
+-------------------------------------
+Final UFW Status: sudo ufw status
+23 DENY | 22 ALLOW | Default: DENY
+Firewall Log: All threats blocked!
+
+[Program finished]
